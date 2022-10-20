ee359cae6eb34df856ec708a42d9231747bbf966
Francisco Collette <franciscocollette@gmail.com>
2022-10-20 01:22:40 +0100

    commit 2

--- js/validacion.js
+++ js/validacion.js
@@ -13,17 +13,53 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
  function validarFormulario(evento) { 
+    
     evento.preventDefault();
-   document.getElementById('formulario').classList.add('was-validated')
+  // document.getElementById('formulario').classList.add('was-validated')
 
-   var password2 = document.getElementById('password2');
-   var password1 = document.getElementById('password1');
-   if(password1.value !== password2.value) {
-    console.log('contrasenias distintas!!')
-    password1.classList.add('is-invalid')
-    return;
+
+  
+   if(pass1.value !== pass2.value || pass1.value.length < 6 ) {
+
+    pass2.classList.add('is-invalid')
+    pass2.classList.remove('is-valid')
+   // return;
 }
-    console.log('SUCCES!!')
+else { pass2.classList.remove('is-invalid')
+pass2.classList.add ('is-valid') }
+
+   if( pass1.value.length > 5 ) { 
+    pass1.classList.add('is-valid')
+    pass1.classList.remove('is-invalid')
+   }
+   else { pass1.classList.remove('is-valid')
+   pass1.classList.add('is-invalid')}
+
+    console.log('bton!!')
+
+    if (nombre.value.length > 0 ) { 
+        nombre.classList.add('is-valid')
+        nombre.classList.remove('is-invalid')
+    }
+    else { nombre.classList.remove('is-valid')
+    nombre.classList.add('is-invalid')}
+
+    
+    if (lastname.value.length > 0 ) { 
+        lastname.classList.remove('is-invalid')
+        lastname.classList.add('is-valid')
+    }
+    else { lastname.classList.remove('is-valid')
+    lastname.classList.add('is-invalid')}
+
+    if (email.value.length > 0  && email.value.includes('@') && email.value.includes('.') ) { 
+        email.classList.remove('is-invalid')
+        email.classList.add('is-valid')
+    }
+    else { email.classList.remove('is-valid')
+    email.classList.add('is-invalid')}
+
+
 }
 
 
